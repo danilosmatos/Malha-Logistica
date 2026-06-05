50a7f3faabaf423a143806cb87181a7c74d1d41a
Anton-Gabriel-code <antoniothecode@gmail.com>
2026-06-05 20:32:35 -0300

    bfs sem biblioteca

--- src/bfs.py
+++ src/bfs.py
@@ -1,1 +1,22 @@
-﻿
+﻿from grafo import Grafo
+
+def bfs(grafo, no1):
+    visitados = []
+    fila = [no1]
+    visitados.append(no1)
+
+    ordem_visita = []
+
+    while len(fila) > 0:
+        no_atual  = fila.pop(0)
+
+        ordem_visita.append(no_atual)
+
+        for vizinho in grafo.vizinhos[no_atual]:
+            if vizinho not in visitados:
+                visitados.append(vizinho)
+                fila.append(vizinho)
+    print(" -> ".join(ordem_visita))
+
+    return visitados
+
